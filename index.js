86484fd19270930b213bad9c759a442af3a65686
Nick Novitski <github@nicknovitski.com>
2015-08-31 23:57:56 +0000

    use console.error for errors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ AWS.config.apiVersions = {
 }
 
 var debugCB = function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
+  if (err) console.error(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
 }
 
